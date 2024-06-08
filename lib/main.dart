799b8c46d3c3c2c421f884aaaae9ea867b36f5f2
Huy17092002 <huylequang1792002@gmail.com>
2024-06-08 11:49:06 +0700

    push code

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:todolist/Home/Home.dart';
+import 'package:flutter/material.dart';
+
+void main (){
+  runApp(TodoListApp());
+}
+
+
+
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'classWidget.dart';
+import 'widget/classWidget.dart';
+import 'screens/class DetailScreen.dart';
 
 class TaskListWidget extends StatelessWidget {
   const TaskListWidget({super.key});
@@ -126,6 +127,54 @@ class ListAddtask extends StatelessWidget {
     );
   }
 }
+
+class Chitiet extends StatelessWidget {
+  const Chitiet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  GestureDetector(
+      onTap: () {
+        showModalBottomSheet(
+          context: context,
+          isScrollControlled: true,
+          builder: (BuildContext context) {
+            return const DetailsScreen();
+          },
+        );
+      },
+      child: Container(
+        height: 45,
+        width: 350,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.grey[300],
+        ),
+        child: const Padding(
+          padding: EdgeInsets.only(top: 2, left: 20),
+          child: Row(
+            children: [
+              Text(
+                'Chi tiết',
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              SizedBox(width: 238),
+              Icon(
+                Icons.arrow_forward_ios,
+                size: 19,
+                color: Colors.grey,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 
 class RepeatSection extends StatelessWidget {
   const RepeatSection({super.key});
