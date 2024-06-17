a351f58735a827e61aa2ddd6aa5f13e6275da1ee
Huy17092002 <huylequang1792002@gmail.com>
2024-06-17 08:58:09 +0700

    push code

--- lib/app_configs.dart
+++ lib/app_configs.dart
@@ -15,47 +15,6 @@ class AppConfigs {
   static const Color trailingIconColor = Colors.grey;
   static const Color textButtonColor = Colors.blue;
 
-  // Text Styles
-  static const TextStyle appBarTextStyle = TextStyle(
-    fontSize: 18,
-    color: primaryColor,
-    fontWeight: FontWeight.bold,
-  );
-
-  static const TextStyle listTitleTextStyle = TextStyle(
-    fontSize: 18,
-    fontWeight: FontWeight.normal,
-  );
-
-  static const TextStyle hintTextStyle = TextStyle(
-    fontSize: 17,
-    color: Colors.grey,
-  );
-
-  static const TextStyle cancelButtonTextStyle = TextStyle(
-    fontSize: 20,
-    color: textButtonColor,
-    fontWeight: FontWeight.w500,
-  );
-
-  static const TextStyle doneButtonTextStyle = TextStyle(
-    fontSize: 18,
-    color: textButtonColor,
-  );
-
-  static const TextStyle appBarMainTitleTextStyle = TextStyle(
-    color: Colors.black,
-    fontSize: 20,
-    fontWeight: FontWeight.bold,
-    height: 1.5,
-  );
-
-  static const TextStyle addButtonTextStyle = TextStyle(
-    fontSize: 17,
-    color: primaryColor,
-    fontWeight: FontWeight.bold,
-  );
-
   // Icon Sizes
   static const double iconSize = 38;
   static const double trailingIconSize = 17;
