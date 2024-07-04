586e0c3954a4db838d00c88b78b6414a572f428b
Huy17092002 <huylequang1792002@gmail.com>
2024-07-04 10:26:17 +0700

    push code

--- lib/home/searchbar_home.dart
+++ lib/home/searchbar_home.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:todolist/configs.dart';
-import 'package:todolist/home/search/searchbarcontroller.dart';
 
 class SearchBarWidget extends StatefulWidget {
-  final SearchBarController paramater;
-
   const SearchBarWidget({
-    required this.paramater,
     super.key,
   });
 
@@ -15,6 +11,44 @@ class SearchBarWidget extends StatefulWidget {
 }
 
 class _SearchBarWidgetState extends State<SearchBarWidget> {
+  late bool isSearch;
+  late bool isFix;
+  late double opacityFix;
+  late double opacityCancel;
+  late TextEditingController myController;
+  late FocusNode focusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    isSearch = false;
+    isFix = false;
+    opacityFix = 1;
+    opacityCancel = 0;
+    myController = TextEditingController();
+    focusNode = FocusNode();
+  }
+
+  void cancel() {
+    setState(() {
+      isSearch = false;
+      isFix = false;
+      opacityFix = 1;
+      opacityCancel = 0;
+    });
+    myController.clear();
+    focusNode.unfocus();
+  }
+
+  void onTapSearch() {
+    setState(() {
+      isSearch = true;
+      isFix = true;
+      opacityCancel = 1;
+      opacityFix = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -22,19 +56,15 @@ class _SearchBarWidgetState extends State<SearchBarWidget> {
         Padding(
           padding: const EdgeInsets.only(left: 303),
           child: GestureDetector(
-            onTap: () {
-              setState(() {
-                widget.paramater.onTapSearch();
-              });
-            },
+            onTap: onTapSearch,
             child: AnimatedContainer(
-              width: widget.paramater.isFix ? 40 : 55,
-              height: widget.paramater.isFix ? 0 : 33,
+              width: isFix ? 40 : 55,
+              height: isFix ? 0 : 33,
               duration: kDuration,
               curve: Curves.easeOutQuad,
               alignment: Alignment.center,
               child: AnimatedOpacity(
-                opacity: widget.paramater.opacityFix,
+                opacity: opacityFix,
                 duration: kDuration,
                 curve: Curves.easeOutCirc,
                 child: TextButton(
@@ -73,43 +103,39 @@ class _SearchBarWidgetState extends State<SearchBarWidget> {
           children: [
             GestureDetector(
               child: AnimatedContainer(
-                width: widget.paramater.isFix ? 305 : 360,
+                width: isFix ? 305 : 360,
                 height: 35,
                 duration: kDuration,
                 curve: Curves.easeOutQuad,
                 decoration: BoxDecoration(
                   color: Colors.grey[300],
-                  borderRadius: BorderRadius.circular(11),
+                  borderRadius: BorderRadius.circular(10),
                 ),
                 child: Row(
                   crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
                     Expanded(
                       child: TextField(
-                        focusNode: widget.paramater.focusNode,
-                        controller: widget.paramater.myController,
+                        focusNode: focusNode,
+                        controller: myController,
                         decoration: const InputDecoration(
                           prefixIcon: Icon(Icons.search),
                           hintText: 'Tìm kiếm',
                           border: InputBorder.none,
                           contentPadding: EdgeInsets.only(bottom: 12.5),
                         ),
-                        onTap: () {
-                          setState(() {
-                            widget.paramater.onTapSearch();
-                          });
-                        },
+                        onTap: onTapSearch,
                       ),
                     ),
-                    if (widget.paramater.isSearch)
+                    if (isSearch)
                       GestureDetector(
                         onTap: () {
                           setState(() {
-                            widget.paramater.myController.clear();
+                            myController.clear();
                           });
                         },
                         child: Padding(
-                          padding: const EdgeInsets.only(bottom: 9, right: 4),
+                          padding: const EdgeInsets.only(bottom: 8.5, right: 4),
                           child: Icon(
                             Icons.cancel,
                             color: Colors.grey[500],
@@ -121,20 +147,16 @@ class _SearchBarWidgetState extends State<SearchBarWidget> {
                 ),
               ),
             ),
-            if (widget.paramater.isSearch)
+            if (isSearch)
               Flexible(
                 child: Padding(
                   padding: const EdgeInsets.only(left: 8),
                   child: GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        widget.paramater.cancel();
-                      });
-                    },
+                    onTap: cancel,
                     child: const Text(
                       'Hủy',
                       style: TextStyle(
-                        fontSize: 18,
+                        fontSize: 20,
                         color: Colors.blue,
                         fontWeight: FontWeight.w500,
                       ),
