6df74ac5c1e4226cfded870c6ad1cb445dd13a82
Huy17092002 <huylequang1792002@gmail.com>
2024-09-19 15:19:14 +0700

    push

---
+++ lib/view/home/homepage/home_bottom_navigationbar.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+
+import '../bottomsheet/addlist_bottomsheet.dart';
+import '../bottomsheet/newreminder_bottomsheet.dart';
+
+class HomeBottomNavigationBar extends StatelessWidget {
+  const HomeBottomNavigationBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 33),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          TextButton.icon(
+            icon: const Icon(
+              Icons.add_circle,
+              size: 30,
+              color: Colors.blue,
+            ),
+            label: const Text(
+              'Lời nhắc mới',
+              style: TextStyle(
+                fontSize: 17,
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onPressed: () {
+              showModalBottomSheet(
+                isScrollControlled: true,
+                context: context,
+                builder: (BuildContext context) {
+                  return const NewReminderBottomsheet();
+                },
+              );
+            },
+          ),
+          const SizedBox(width: 45),
+          TextButton(
+            child: const Text(
+              'Thêm danh sách',
+              style: TextStyle(
+                fontSize: 17,
+                color: Colors.blue,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            onPressed: () {
+              showModalBottomSheet(
+                context: context,
+                isScrollControlled: true,
+                builder: (BuildContext context) {
+                  return const AddListBottomsheet();
+                },
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/view/home/homepage/home_searchbar.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'package:todolist/configs.dart';
+
+class HomeSearchBar extends StatefulWidget {
+  const HomeSearchBar({super.key});
+
+  @override
+  State<HomeSearchBar> createState() => _HomeSearchBarState();
+}
+
+class _HomeSearchBarState extends State<HomeSearchBar> {
+  late bool isSearch;
+  late bool isFix;
+  late double opacityFix;
+  late double opacityCancel;
+  late TextEditingController myController;
+  late FocusNode focusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    isSearch = false;
+    isFix = false;
+    opacityFix = 1;
+    opacityCancel = 0;
+    myController = TextEditingController();
+    focusNode = FocusNode();
+  }
+
+  void cancel() {
+    setState(() {
+      isSearch = false;
+      isFix = false;
+      opacityFix = 1;
+      opacityCancel = 0;
+    });
+    myController.clear();
+    focusNode.unfocus();
+  }
+
+  void onTapSearch() {
+    setState(() {
+      isSearch = true;
+      isFix = true;
+      opacityCancel = 1;
+      opacityFix = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 303),
+          child: GestureDetector(
+            onTap: onTapSearch,
+            child: AnimatedContainer(
+              width: isFix ? 40 : 55,
+              height: isFix ? 0 : 33,
+              duration: kDuration,
+              curve: Curves.easeOutQuad,
+              alignment: Alignment.center,
+              child: AnimatedOpacity(
+                opacity: opacityFix,
+                duration: kDuration,
+                curve: Curves.easeOutCirc,
+                child: TextButton(
+                  child:const Text(
+                    'Sửa',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text('Thank you very much'),
+                          actions: [
+                            ElevatedButton(
+                              child: const Text('OK'),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
+        ),
+        Row(
+          children: [
+            GestureDetector(
+              child: AnimatedContainer(
+                width: isFix ? 305 : 360,
+                height: 35,
+                duration: kDuration,
+                curve: Curves.easeOutQuad,
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        focusNode: focusNode,
+                        controller: myController,
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(Icons.search),
+                          hintText: 'Tìm kiếm',
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.only(bottom: 12.5),
+                        ),
+                        onTap: onTapSearch,
+                      ),
+                    ),
+                    if (isSearch)
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            myController.clear();
+                          });
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.only(bottom: 8.5, right: 4),
+                          child: Icon(
+                            Icons.cancel,
+                            color: Colors.grey[500],
+                            size: 17,
+                          ),
+                        ),
+                      )
+                  ],
+                ),
+              ),
+            ),
+            if (isSearch)
+              Flexible(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 8),
+                  child: GestureDetector(
+                    onTap: cancel,
+                    child: const Text(
+                      'Hủy',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.blue,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+          ],
+        )
+      ],
+    );
+  }
+}
