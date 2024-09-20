85e0cd333dcaee65a2357883f5ee5ca08c5d0665
Huy17092002 <huylequang1792002@gmail.com>
2024-09-20 09:23:35 +0700

    push

--- lib/widget/priority_selector.dart
+++ lib/widget/priority_selector.dart
@@ -1,79 +1,3 @@
-// import 'package:flutter/material.dart';
-//
-// class PrioritySelector extends StatefulWidget {
-//   PrioritySelector({super.key});
-//
-//   final List<String> list = <String>['None', 'Low', 'Medium', 'High'];
-//
-//   @override
-//   State<PrioritySelector> createState() => _PrioritySelectorState();
-// }
-//
-// class _PrioritySelectorState extends State<PrioritySelector> {
-//   late String dropdownValue;
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     dropdownValue = widget.list.first;
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       height: 45,
-//       width: 350,
-//       decoration: BoxDecoration(
-//         borderRadius: BorderRadius.circular(10),
-//         color: Colors.grey[300],
-//       ),
-//       child: Row(
-//         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//         children: [
-//           Padding(
-//             padding: const EdgeInsets.only(left: 15),
-//             child: Row(
-//               children: [
-//                 Container(
-//                   width: 30,
-//                   height: 30,
-//                   decoration: BoxDecoration(
-//                     shape: BoxShape.rectangle,
-//                     color: Colors.red,
-//                     borderRadius: BorderRadius.circular(5),
-//                   ),
-//                   child: const Icon(Icons.priority_high,
-//                       color: Colors.white, size: 20),
-//                 ),
-//                 const SizedBox(width: 10),
-//                 const Text(
-//                   'Mức ưu tiên',
-//                   style: TextStyle(fontSize: 18),
-//                 ),
-//               ],
-//             ),
-//           ),
-//           DropdownButton<String>(
-//             value: dropdownValue,
-//             onChanged: (String? newValue) {
-//               setState(() {
-//                 dropdownValue = newValue!;
-//               });
-//             },
-//             items: widget.list.map<DropdownMenuItem<String>>((String value) {
-//               return DropdownMenuItem<String>(
-//                 value: value,
-//                 child: Text(value,style: TextStyle(fontWeight: FontWeight.normal),),
-//               );
-//             }).toList(),
-//             underline: Container(),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 
 class PrioritySelector extends StatefulWidget {
@@ -131,7 +55,6 @@ class _PrioritySelectorState extends State<PrioritySelector> {
           ),
           Row(
             children: [
-
               DropdownButton<String>(
                 value: dropdownValue,
                 onChanged: (String? newValue) {
