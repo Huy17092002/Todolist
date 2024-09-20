40d5750a03ea80b75f66b394b99494c87534b8e3
Huy17092002 <huylequang1792002@gmail.com>
2024-09-20 10:12:50 +0700

    crete object tasklistcollection

--- lib/view/home/homepage/home_page.dart
+++ lib/view/home/homepage/home_page.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
+import '../../../model/tasklist.dart';
 import '../../../model/tasklist_collection.dart';
+import '../../../routes.dart';
+import '../../component/items/task_group_item.dart';
 import 'home_bottom_navigationbar.dart';
-import 'home_list.dart';
 import 'home_searchbar.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({super.key});
 
   final TaskListCollection data = TaskListCollection(
-    title: 'Danh sach cua toi',
-    tasklists: [],
+    title: 'Danh sách của tôi',
+    tasklists: [
+      TaskList(title: 'Facebook', icon: Icons.facebook, color: Colors.blue, tasks: []),
+      TaskList(title: 'TikTok',icon: Icons.tiktok, color: Colors.black, tasks: []),
+      TaskList(title: 'Telegram',icon: Icons.telegram, color: Colors.blue.shade400, tasks: []),
+      TaskList(title: 'Dien Thoai',icon: Icons.phone_iphone, color: Colors.red, tasks: []),
+      TaskList(title: 'Camera',icon: Icons.camera_alt, color: Colors.grey, tasks: []),
+      TaskList(title: 'Bong', tasks: []),
+      TaskList(title: 'Danh Sach', color: Colors.yellowAccent, tasks: []),
+      TaskList(title: 'Boi', tasks: []),
+    ],
   );
 
   @override
@@ -43,11 +54,35 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
             const SizedBox(height: 5),
-            HomeList(),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black26, width: 0.24),
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: widget.data.tasklists.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final item = widget.data.tasklists[index];
+                  return TaskGroupItem(
+                    onTap: () {
+                      print('${item.title} $index');
+                      Navigator.pushNamed(context, Routes.addTask);
+                    },
+                    model: item,
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
       bottomNavigationBar: const HomeBottomNavigationBar(),
     );
   }
+
 }
+
+
