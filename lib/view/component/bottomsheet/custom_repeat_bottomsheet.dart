c3edc310792d7e5272cc6241da42ddc01bdf6faf
Huy17092002 <huylequang1792002@gmail.com>
2024-09-24 16:48:35 +0700

    complete interface

--- lib/view/component/bottomsheet/custom_repeat_bottomsheet.dart
+++ lib/view/component/bottomsheet/custom_repeat_bottomsheet.dart
@@ -1,8 +1,24 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:todolist/configs.dart';
 
-class  CustomRepeatBottomsheet extends StatelessWidget {
-  const  CustomRepeatBottomsheet({super.key});
+class CustomRepeatBottomsheet extends StatefulWidget {
+  const CustomRepeatBottomsheet({super.key});
+
+  @override
+  CustomRepeatBottomsheetState createState() => CustomRepeatBottomsheetState();
+}
+
+class CustomRepeatBottomsheetState extends State<CustomRepeatBottomsheet> {
+  bool _isExpanded = false;
+  int _selectedValue = 0 ;
+
+  get index => null;
+
+  void _toggleExpand() {
+    setState(() {
+      _isExpanded = !_isExpanded;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,11 +50,9 @@ class  CustomRepeatBottomsheet extends StatelessWidget {
                           color: Colors.blue,
                         ),
                       ),
-                      SizedBox(
-                        width: 70,
-                      ),
+                      SizedBox(width: 70),
                       Text(
-                        'Custom',
+                        'Tùy chỉnh',
                         style: TextStyle(
                           fontSize: 18,
                           fontWeight: FontWeight.bold,
@@ -53,53 +67,97 @@ class  CustomRepeatBottomsheet extends StatelessWidget {
           body: Column(
             children: [
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 25),
-                child: Container(
-                  height: 110,
+                padding: const EdgeInsets.only(top: 25,left: 16,right: 16),
+                child: AnimatedContainer(
+                  duration: const Duration(milliseconds: 300),
+                  height: _isExpanded ? 300 : 128,
                   width: 360,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(8),
                     color: Colors.grey[300],
                   ),
-                  child: ListView(
-                    padding: const EdgeInsets.only(top: 1),
-                    children: const [
-                      Column(
-                        children: [
-                          ListTile(
-                            title: Text(
-                              'Tần suất',
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.normal,
-                              ),
-                            ),
-                            trailing: Text('Daily',
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: Colors.blue,
-                              ),
-                            ),
+                  child: Column(
+                    children: [
+                      const ListTile(
+                        title: Text(
+                          'Tần suất',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.normal,
                           ),
-                          dividerRepeatBottomSheet,
-                          ListTile(
-                            title: Text(
-                              'Mỗi',
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.normal,
-                              ),
-                            ),
-                            trailing: Text('Day',
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: Colors.blue,
-                              ),
-                            ),
+                        ),
+                        trailing: Text(
+                          'Hàng ngày ',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.blue,
                           ),
-                        ],
+                        ),
                       ),
+                      const Divider(
+                        indent: 17,
+                      ),
+                      ListTile(
+                        title: const Text(
+                          'Mỗi',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                        trailing: Text(
+                          '$_selectedValue Ngày',
+                          style: const TextStyle(
+                            fontSize: 16,
+                            color: Colors.blue,
+                          ),
+                        ),
+                        onTap: _toggleExpand,
+                      ),
+                      if (_isExpanded)
+                        Expanded(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SizedBox(
+                                width: 300,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Expanded(
+                                      child: CupertinoPicker(
+                                        itemExtent: 40.0,
+                                        onSelectedItemChanged: (int index) {
+                                          setState(() {
+                                            _selectedValue = index;
+                                          });
+                                        },
+                                        children: List<Widget>.generate(1000, (int index) {
+                                          return Center(child: Text('$index'));
+                                        }),
+                                      ),
+                                    ),
+                                    const Text(
+                                      'Ngày',
+                                      style: TextStyle(fontSize: 24, color: Colors.black),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
                     ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 24,bottom: 180),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Sự kiện sẽ diễn ra sau mỗi $_selectedValue ngày',
+                    style: const TextStyle(fontSize: 12,color: Colors.black,),
                   ),
                 ),
               ),
@@ -110,3 +168,6 @@ class  CustomRepeatBottomsheet extends StatelessWidget {
     );
   }
 }
+
+
+
