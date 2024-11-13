7eda43e389bcc3b6e0fd3a1ce6dabf002326f25b
lqh <huylequang1792002@gmail.com>
2024-11-13 14:56:23 +0700

    push code

--- lib/view/home/homepage/home_page.dart
+++ lib/view/home/homepage/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:todolist/model/tasklist.dart';
 import 'package:todolist/view/component/items/task_list_item.dart';
 import 'package:todolist/view/home/homepage/home_bottom_navigationbar.dart';
 import 'package:todolist/view/home/homepage/home_searchbar.dart';
@@ -17,70 +18,94 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    Provider.of<TaskListCollectionViewModel>(context, listen: false).getTaskListCollection();
+    context.read<TaskListCollectionViewModel>().getTaskListCollection();
   }
 
   @override
   Widget build(BuildContext context) {
-    final taskListCollectionViewModel =
-        Provider.of<TaskListCollectionViewModel>(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
         toolbarHeight: 67,
         title: const HomeSearchBar(),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 17, top: 15),
-              child: Text(
-                taskListCollectionViewModel.taskListCollection?.title ??
-                    'Danh sách của tôi',
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  height: 1.5,
-                  color: Colors.black,
-                ),
+      body: Consumer<TaskListCollectionViewModel>(
+          builder: (context, taskListCollectionViewModel, child) {
+        if (taskListCollectionViewModel.isLoading) {
+          return const Center(
+            child: CircularProgressIndicator(
+              color: Colors.red,
+            ),
+          );
+        }
+
+        if (taskListCollectionViewModel.error != null) {
+          return Center(
+            child: Text(
+              taskListCollectionViewModel.error ?? 'lỗi không tải được',
+              style: const TextStyle(
+                fontSize: 24,
+                color: Colors.red,
               ),
             ),
-            const SizedBox(height: 5),
-            Padding(
-              padding: const EdgeInsets.only(left: 15, right: 15),
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.white70),
-                  color: Colors.grey[200],
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: taskListCollectionViewModel.taskListCollection?.tasklists.length ?? 0,
-                  itemBuilder: (BuildContext context, int index) {
-                    final item = taskListCollectionViewModel.taskListCollection!.tasklists[index];
-                    return TaskListItem(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => TaskListPage(taskList: item),
-                          ),
-                        );
-                      },
-                      model: item,
-                    );
-                  },
+          );
+        }
+        return SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 17, top: 15),
+                child: Text(
+                  taskListCollectionViewModel.taskListCollection?.title ??
+                      'Danh sách của tôi',
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    height: 1.5,
+                    color: Colors.black,
+                  ),
                 ),
               ),
-            ),
-          ],
-        ),
-      ),
+              const SizedBox(height: 5),
+              Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.white70),
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: taskListCollectionViewModel.taskListCollection?.tasklists.length ?? 0,
+                    itemBuilder: (BuildContext context, int index) {
+                      final item = taskListCollectionViewModel.taskListCollection?.tasklists[index] ??
+                          TaskList(title: '', tasks: []);
+                      return TaskListItem(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  TaskListPage(taskList: item),
+                            ),
+                          );
+                        },
+                        model: item,
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      }),
       bottomNavigationBar: const HomeBottomNavigationBar(),
     );
   }
 }
+
+
--- lib/viewmodel/tasklistcollection_viewmodel.dart
+++ lib/viewmodel/tasklistcollection_viewmodel.dart
@@ -6,110 +6,122 @@ import 'package:todolist/model/tasklist_collection.dart';
 
 class TaskListCollectionViewModel extends ChangeNotifier {
   TaskListCollection? taskListCollection;
+  bool isLoading = false;
+  String? error;
 
   Future<void> getTaskListCollection() async {
-    await Future.delayed(const Duration(milliseconds: 500));
-    taskListCollection = TaskListCollection(
-      title: 'Danh sách của tôi',
-      tasklists: [
-        TaskList(
-          title: 'Facebook',
-          icon: Icons.facebook,
-          color: Colors.blue,
-          tasks: [
-            Task(
-              title: 'New Reminder',
-              description: "Vùng",
-              isCompleted: false,
-              deadline: '10/09/2024 15:00',
-              repeat: 'Daily',
-              priority: Priority.medium,
-            ),
-            Task(
-              title: 'Cat',
-              description: 'Mèo',
-              isCompleted: false,
-              deadline: '10/09/2024 09:00',
-              repeat: 'Daily',
-              priority: Priority.high,
-            ),
-            Task(
-              title: 'Birth',
-              description: 'Chim',
-              isCompleted: false,
-              deadline: '10/09/2024 15:00',
-              repeat: 'Daily',
-              priority: Priority.low,
-            ),
-            Task(title: 'egj', isCompleted: true, description: 'heperlink'),
-          ],
-        ),
-        TaskList(
-          title: 'TikTok',
-          icon: Icons.tiktok,
-          color: Colors.black,
-          tasks: [
-            Task(
-              title: 'Gạo',
-              description: 'Bắc',
-              isCompleted: false,
-              deadline: '17/09/2024',
-              repeat: '',
-              priority: Priority.high,
-            ),
-          ],
-        ),
-        TaskList(
-          title: 'Telegram',
-          icon: Icons.telegram,
-          color: Colors.blue.shade400,
-          tasks: [
-            Task(
-              title: 'Trứng',
-              description: 'Egg',
-              isCompleted: true,
-              deadline: '20/01/2024',
-            ),
-          ],
-        ),
-        TaskList(
-          title: 'Điện Thoại',
-          icon: Icons.phone_iphone,
-          color: Colors.red,
-          tasks: [
-            Task(
-              title: 'New Reminder',
-              description: 'Party',
-              isCompleted: false,
-              deadline: '10/09/2024 15:00',
-              repeat: 'Daily',
-              priority: Priority.low,
-            ),
-          ],
-        ),
-        TaskList(
-          title: 'Camera',
-          icon: Icons.camera_alt,
-          color: Colors.grey,
-          tasks: [
-            Task(
-              title: 'Đá bóng',
-              description: 'Ronaldo',
-              isCompleted: false,
-              deadline: '17/09/2024',
-              repeat: '',
-              priority: Priority.high,
-            ),
-            Task(
-              title: 'Cá',
-              description: 'Fish',
-              isCompleted: true,
-              deadline: '20/01/2024',
-            ),
-          ],
-        ),
-      ],
-    );
+    isLoading = true;
+    error = null;
+    try {
+      await Future.delayed(const Duration(seconds: 2));
+      taskListCollection = TaskListCollection(
+        title: 'Danh sách của tôi',
+        tasklists: [
+          TaskList(
+            title: 'Facebook',
+            icon: Icons.facebook,
+            color: Colors.blue,
+            tasks: [
+              Task(
+                title: 'New Reminder',
+                description: "Vùng",
+                isCompleted: false,
+                deadline: '10/09/2024 15:00',
+                repeat: 'Daily',
+                priority: Priority.medium,
+              ),
+              Task(
+                title: 'Cat',
+                description: 'Mèo',
+                isCompleted: false,
+                deadline: '10/09/2024 09:00',
+                repeat: 'Daily',
+                priority: Priority.high,
+              ),
+              Task(
+                title: 'Birth',
+                description: 'Chim',
+                isCompleted: false,
+                deadline: '10/09/2024 15:00',
+                repeat: 'Daily',
+                priority: Priority.low,
+              ),
+              Task(title: 'egj', isCompleted: true, description: 'heperlink'),
+            ],
+          ),
+          TaskList(
+            title: 'TikTok',
+            icon: Icons.tiktok,
+            color: Colors.black,
+            tasks: [
+              Task(
+                title: 'Gạo',
+                description: 'Bắc',
+                isCompleted: false,
+                deadline: '17/09/2024',
+                repeat: '',
+                priority: Priority.high,
+              ),
+            ],
+          ),
+          TaskList(
+            title: 'Telegram',
+            icon: Icons.telegram,
+            color: Colors.blue.shade400,
+            tasks: [
+              Task(
+                title: 'Trứng',
+                description: 'Egg',
+                isCompleted: true,
+                deadline: '20/01/2024',
+              ),
+            ],
+          ),
+          TaskList(
+            title: 'Điện Thoại',
+            icon: Icons.phone_iphone,
+            color: Colors.red,
+            tasks: [
+              Task(
+                title: 'New Reminder',
+                description: 'Party',
+                isCompleted: false,
+                deadline: '10/09/2024 15:00',
+                repeat: 'Daily',
+                priority: Priority.low,
+              ),
+            ],
+          ),
+          TaskList(
+            title: 'Camera',
+            icon: Icons.camera_alt,
+            color: Colors.grey,
+            tasks: [
+              Task(
+                title: 'Đá bóng',
+                description: 'Ronaldo',
+                isCompleted: false,
+                deadline: '17/09/2024',
+                repeat: '',
+                priority: Priority.high,
+              ),
+              Task(
+                title: 'Cá',
+                description: 'Fish',
+                isCompleted: true,
+                deadline: '20/01/2024',
+              ),
+            ],
+          ),
+        ],
+      );
+      if(taskListCollection?.tasklists.isEmpty == true){
+        throw Exception();
+      }
+    }catch(errol) {
+      error = 'lỗi không tải được dữ liệu!';
+    }
+    isLoading = false;
     notifyListeners();
   }
 
