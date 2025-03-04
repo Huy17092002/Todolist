c5d55ea3db329f4997ec35485af2f8914c7db2c6
lqh <huylequang1792002@gmail.com>
2025-03-04 11:53:56 +0700

    push

--- lib/view/task/bottomsheet/detailstask_bottomsheet.dart
+++ lib/view/task/bottomsheet/detailstask_bottomsheet.dart
@@ -1,183 +1,14 @@
-// import 'package:flutter/material.dart';
-// import 'package:todolist/view/component/bottomsheet/repeatintervalltime_bottomsheet.dart';
-// import 'package:todolist/widget/datetime_picker.dart';
-// import 'package:todolist/widget/location_selector.dart';
-// import 'package:todolist/widget/priority_selector.dart';
-//
-// class DetailsTaskBottomsheet extends StatefulWidget {
-//   const DetailsTaskBottomsheet({super.key});
-//
-//   @override
-//   State<DetailsTaskBottomsheet> createState() => _DetailsTaskBottomsheetState();
-// }
-//
-// class _DetailsTaskBottomsheetState extends State<DetailsTaskBottomsheet> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return ClipRRect(
-//       borderRadius: const BorderRadius.only(
-//         topLeft: Radius.circular(10),
-//         topRight: Radius.circular(10),
-//       ),
-//       child: Container(
-//         color: Colors.grey,
-//         height: 780,
-//         width: 500,
-//         child: Scaffold(
-//           appBar: AppBar(
-//             leadingWidth: 400,
-//             leading: Row(
-//               children: [
-//                 const Padding(padding: EdgeInsets.only(left: 10)),
-//                 GestureDetector(
-//                   onTap: () {
-//                     Navigator.pop(context);
-//                   },
-//                   child: const Row(
-//                     children: [
-//                       Icon(
-//                         Icons.arrow_back_ios,
-//                         color: Colors.blue,
-//                       ),
-//                       Text(
-//                         'Lời nhắc mới',
-//                         style: TextStyle(
-//                           fontSize: 17,
-//                           color: Colors.blue,
-//                           fontWeight: FontWeight.w500,
-//                         ),
-//                       ),
-//                     ],
-//                   ),
-//                 ),
-//                 const SizedBox(width: 30),
-//                 const Text(
-//                   'Chi Tiết',
-//                   style: TextStyle(
-//                     fontSize: 18,
-//                     fontWeight: FontWeight.bold,
-//                   ),
-//                 ),
-//                 const SizedBox(width: 70),
-//                 TextButton(
-//                   child: const Text(
-//                     'Thêm',
-//                     style: TextStyle(fontSize: 18, color: Colors.blue, fontWeight: FontWeight.bold),
-//                   ),
-//                   onPressed: () {
-//                     showDialog(
-//                       context: context,
-//                       builder: (BuildContext context) {
-//                         return AlertDialog(
-//                           title: const Text('Xong'),
-//                           actions: [
-//                             ElevatedButton(
-//                               child: const Text('OK'),
-//                               onPressed: () {
-//                                 Navigator.pop(context);
-//                               },
-//                             ),
-//                           ],
-//                         );
-//                       },
-//                     );
-//                   },
-//                 ),
-//               ],
-//             ),
-//           ),
-//           body: SingleChildScrollView(
-//             child: Padding(
-//               padding: const EdgeInsets.only(top: 10),
-//               child: Container(
-//                 padding: const EdgeInsets.symmetric(horizontal: 21),
-//                 child: Column(
-//                   children: [
-//                     DateTimePicker(
-//                     ),
-//                     const SizedBox(height: 20),
-//                     GestureDetector(
-//                       onTap: () {
-//                         showModalBottomSheet(
-//                           context: context,
-//                           isScrollControlled: true,
-//                           builder: (BuildContext context) {
-//                             return const RepeatIntervallTime();
-//                           },
-//                         );
-//                       },
-//                       child: Container(
-//                         height: 45,
-//                         width: 350,
-//                         decoration: BoxDecoration(
-//                           borderRadius: BorderRadius.circular(10),
-//                           color: Colors.grey[300],
-//                         ),
-//                         child: const Padding(
-//                           padding: EdgeInsets.only(top: 0, left: 15),
-//                           child: Row(
-//                             children: [
-//                               Icon(
-//                                 Icons.repeat_on,
-//                                 color: Colors.grey,
-//                                 size: 30,
-//                               ),
-//                               SizedBox(width: 10),
-//                               Text(
-//                                 'Lặp lại',
-//                                 style: TextStyle(
-//                                   fontSize: 17,
-//                                   fontWeight: FontWeight.w500,
-//                                 ),
-//                               ),
-//                               SizedBox(width: 160),
-//                               Text(
-//                                 'Không',
-//                                 style: TextStyle(
-//                                   fontSize: 17,
-//                                   color: Colors.grey,
-//                                 ),
-//                               ),
-//                               SizedBox(width: 1),
-//                               Icon(
-//                                 Icons.arrow_forward_ios,
-//                                 color: Colors.grey,
-//                                 size: 16,
-//                               ),
-//                             ],
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                     const SizedBox(height: 20),
-//                     const LocationEnable(),
-//                     const SizedBox(height: 20),
-//                     PrioritySelector(),
-//                   ],
-//                 ),
-//               ),
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-
-
 import 'package:flutter/material.dart';
+import 'package:todolist/model/task.dart';
 import 'package:todolist/model/tasklist.dart';
 import 'package:todolist/widget/repeatintervalltime_bottomsheet.dart';
 import 'package:todolist/widget/datetime_picker.dart';
-import 'package:todolist/widget/location_selector.dart';
 import 'package:todolist/widget/priority_selector.dart';
 
 class DetailsTaskBottomsheet extends StatefulWidget {
-  const DetailsTaskBottomsheet({super.key, required this.taskList});
+  const DetailsTaskBottomsheet({super.key, required this.taskList, required this.task});
   final TaskList taskList;
-
-  get task => null;
+  final Task task;
 
   @override
   State<DetailsTaskBottomsheet> createState() => _DetailsTaskBottomsheetState();
@@ -239,23 +70,8 @@ class _DetailsTaskBottomsheetState extends State<DetailsTaskBottomsheet> {
                     style: TextStyle(fontSize: 18, color: Colors.blue, fontWeight: FontWeight.bold),
                   ),
                   onPressed: () {
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: const Text('Xong'),
-                          actions: [
-                            ElevatedButton(
-                              child: const Text('OK'),
-                              onPressed: () {
-                                Navigator.pop(context);
-                              },
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  },
+                    Navigator.pop(context);
+                  }
                 ),
               ],
             ),
@@ -299,7 +115,7 @@ class _DetailsTaskBottomsheetState extends State<DetailsTaskBottomsheet> {
                               Icon(
                                 Icons.repeat_on,
                                 color: Colors.grey,
-                                size: 30,
+                                size: 40,
                               ),
                               SizedBox(width: 10),
                               Text(
@@ -328,8 +144,6 @@ class _DetailsTaskBottomsheetState extends State<DetailsTaskBottomsheet> {
                         ),
                       ),
                     ),
-                    const SizedBox(height: 20),
-                    // const LocationEnable(),
                     const SizedBox(height: 20),
                     PrioritySelector(task: widget.task,),
                   ],
